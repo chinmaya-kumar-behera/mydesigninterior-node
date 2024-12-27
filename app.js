54a09c2bd242a3ca21d5a0800edf7c832570a02e
chinmaya-kumar-behera <official.chinu2127@gmail.com>
2024-12-27 20:58:29 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Set EJS as the template engine
+app.set("view engine", "ejs");
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About Me" });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { title: "Contact" });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
