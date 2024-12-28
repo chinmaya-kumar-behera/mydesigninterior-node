28f38516a9d12f04adfc913063bddcadeaceade5
chinmaya-kumar-behera <official.chinu2127@gmail.com>
2024-12-28 15:36:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 
 app.get("/contact", (req, res) => {
   res.send("Hello from /contact")
-  res.render("contact", { title: "Contact" });
+  // res.render("contact", { title: "Contact" });
 });
 
 // Route to handle form submission
